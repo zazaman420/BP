39cdc6832db590aa134d52a15ce2eaa7fcfe4e50
zazaman420 <153211293+zazaman420@users.noreply.github.com>
2023-12-13 18:03:28 +0000

    Add files via upload

---
+++ BP2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soap;
+
+/**
+ *
+ * @author Racist101
+ */
+public class SOAP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
